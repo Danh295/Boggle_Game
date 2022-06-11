407ba19fa5ab81a9b971406dc357f6d4aeaf05e1
danh295 <hudanny295@gmail.com>
2022-06-11 13:39:15 -0400

    Create Human.java
    Human player: subclass of the player class

---
+++ src/Boggle/Human.java
@@ -0,0 +1,11 @@
+package Boggle;
+
+// todo: comment this class
+public class Human extends Player {
+
+    public Human(String name, int score) {
+        super();
+    }
+
+    public String getID() { return name; }
+}
