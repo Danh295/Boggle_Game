ab9344e235a9e1e73d8146e37c11510860bfc5d0
kimchi-5998 <benjaminsangminkim@gmail.com>
2022-06-13 09:08:34 -0400

    Update GUI.java

--- src/Boggle/GUI.java
+++ src/Boggle/GUI.java
@@ -16,33 +16,38 @@ public class GUI extends JFrame implements ActionListener{
         setName("BOGGLE!");
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        middle = new JPanel(new GridLayout(5, 5));
+
+
+        middle = new JPanel(new GridLayout(5,5));
 
         grid = new JLabel[5][5];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                grid[i][j].setText("Test");
+                grid[i][j] = new JLabel("Test");
+            }
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 middle.add(grid[i][j]);
             }
         }
         add(middle);
 
-        upper = new JPanel(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        lower = new JPanel();
+        lower.setLayout(new BoxLayout(lower, BoxLayout.Y_AXIS));
 
-        title = new JLabel();
-        title.setText("Score");
-        upper.add(title);
+        title = new JLabel("Score");
+        lower.add(title);
 
         scoreBoard = new JPanel(new FlowLayout());
-        p1 = new JLabel();
-        p1.setText("0");    // To update scores, we can do smth like get string value, convert to int, set text everytime
+        p1 = new JLabel("0");
         scoreBoard.add(p1);
-        p2 = new JLabel();
-        p2.setText("0");
+        p2 = new JLabel("0");
         scoreBoard.add(p2);
-        upper.add(scoreBoard);
+        lower.add(scoreBoard);
 
-        add(upper);
+        add(lower);
 
         setVisible(true);
     }
