8fbe45bef4efcfa9de76ffbc7c09db938ef0b2cf
danh295 <hudanny295@gmail.com>
2022-06-13 17:01:05 -0400

    Update Player.java

--- src/Boggle/Player.java
+++ src/Boggle/Player.java
@@ -7,24 +7,25 @@ public abstract class Player {
     String name;
     int score;
 
+    /* accessor methods */
     public abstract String getID();
     public int getScore() { return score; }
+
+    /* modifier methods */
     public void setName(String name) { this.name = name; }
     public void setScore(int score) { this.score = score; }
-    public int addScore(String word) {
+
+    /**
+     * Adds to the player's score depending on the length of the given word
+     * @param word word that the player successfully guessed
+     */
+    public void addScore(String word) {
         int wordLength = word.length();
-        if (wordLength == 3 || wordLength == 4) {
-            this.score += 1;
-        } else if (wordLength == 5) {
-            this.score += 2;
-        } else if (wordLength == 6) {
-            this.score += 3;
-        } else if (wordLength == 7) {
-            this.score += 5;
-        } else if (wordLength >= 9) {
-            this.score += 11;
-        }
-        return this.score;
+        if (wordLength == 3 || wordLength == 4) score += 1;
+        else if (wordLength == 5) score += 2;
+        else if (wordLength == 6) score += 3;
+        else if (wordLength == 7) score += 5;
+        else if (wordLength >= 9) score += 11;
     }
 
 }
