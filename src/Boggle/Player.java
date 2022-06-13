d6c10ff30e31ce23049bb9603158eca41b6a7450
danh295 <hudanny295@gmail.com>
2022-06-13 16:13:48 -0400

    Created addScore method
    conditionally add to the score of the object when called

--- src/Boggle/Player.java
+++ src/Boggle/Player.java
@@ -11,4 +11,21 @@ public abstract class Player {
     public int getScore() { return score; }
     public void setName(String name) { this.name = name; }
     public void setScore(int score) { this.score = score; }
+    public int addScore(String word) {
+        int wordLength = word.length();
+        if (wordLength == 3 || wordLength == 4) {
+            this.score += 1;
+        } else if (wordLength == 5) {
+            this.score += 2;
+        } else if (wordLength == 6) {
+            this.score += 3;
+        } else if (wordLength == 7) {
+            this.score += 5;
+        } else if (wordLength >= 9) {
+            this.score += 11;
+        }
+        return this.score;
+    }
+
 }
+
