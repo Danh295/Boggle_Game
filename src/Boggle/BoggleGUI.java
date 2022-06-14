6bea8ce7330ab5ce54e1f78f988a62cf7e8087e3
danh295 <hudanny295@gmail.com>
2022-06-14 16:54:43 -0400

    Update BoggleGUI.java

--- src/Boggle/BoggleGUI.java
+++ src/Boggle/BoggleGUI.java
@@ -10,8 +10,10 @@ public class BoggleGUI extends JFrame implements ActionListener{
 
     JLabel[][] grid;
     JPanel upper, middle, lower;    //  3 main panels
-    JPanel buttons, scoreBoard;     //  Panels within panels
+    JPanel buttons, scoreBoard, mode;     //  Panels within panels
+    JPanel scores;  //  Panels within the panels within the panels
     JButton start, pause, restart, exit;
+    JButton single, multi;
     JLabel p1, p2, title, instructions;
     JTextField principleValue, minChar;
 
@@ -28,6 +30,7 @@ public class BoggleGUI extends JFrame implements ActionListener{
 
         middle = new JPanel(new FlowLayout());
 
+        // A panel within the middle panel for buttons asking if you want to start, exit, etc.
         buttons = new JPanel(new GridLayout(2, 2));
         start = new JButton("Start");
         buttons.add(start);
@@ -39,13 +42,38 @@ public class BoggleGUI extends JFrame implements ActionListener{
         buttons.add(exit);
         middle.add(buttons);
 
+        // Text fields for user inputs that set the parameters of the game
         principleValue = new JTextField("Target Score");
         middle.add(principleValue);
         minChar = new JTextField("Shortest word allowed");
         middle.add(minChar);
 
+        // Single or multiplayer and if single, choose difficulty
+        mode = new JPanel(new GridLayout(2, 2));
+        single = new JButton("Single Player");
+        mode.add(single);
+        multi = new JButton("Multiplayer");
+        mode.add(multi);
 
 
+        // JPanel to make the scoreboard
+        JPanel scoreBoard = new JPanel();   // JPanel for keeping score chart
+        scoreBoard.setLayout(new BoxLayout(scoreBoard, BoxLayout.Y_AXIS));  // Making it a box layout so it can go from top to bottom
+        title = new JLabel("Score Board");
+        scoreBoard.add(title);
+
+        JPanel scores = new JPanel(new FlowLayout());   // Flow layout because p1 and p2 scores are going to be beside each other
+        p1 = new JLabel("0");
+        scores.add(p1);
+        p2 = new JLabel("0");
+        scores.add(p2);
+        scoreBoard.add(scores);
+
+        middle.add(scoreBoard);
+        add(middle);
+
+
+        // Panel to contain the actual grid
         lower = new JPanel(new GridLayout(5,5));
 
         grid = new JLabel[5][5];
